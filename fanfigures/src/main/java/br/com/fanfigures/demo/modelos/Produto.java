ac9600dcce3b51fb4e70f8c342871ccb228595f4
User <User@172.16.1.225>
2020-03-06 22:03:52 -0300

    Aula_Spring

---
+++ fanfigures/src/main/java/br/com/fanfigures/demo/modelos/Produto.java
@@ -0,0 +1,59 @@
+package br.com.fanfigures.demo.modelos;
+
+import java.math.BigInteger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private int quantidade;
+	private BigInteger valor;
+	private String descricao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public BigInteger getValor() {
+		return valor;
+	}
+
+	public void setValor(BigInteger valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+}
